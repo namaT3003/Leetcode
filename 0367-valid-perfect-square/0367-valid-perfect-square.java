aadaa81ff87c106a79786410f80377d4c2653661
namaT3003 <n.tiwari0138@gmail.com>
2026-07-10 21:10:29 +0530

    Time: 0 ms (100%), Space: 42 MB (55.96%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int left = 1;
+        int right = num;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
+
+            if (square == num) {
+                return true;
+            } else if (square < num) {
+                left = (int) mid + 1;
+            } else {
+                right = (int) mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
