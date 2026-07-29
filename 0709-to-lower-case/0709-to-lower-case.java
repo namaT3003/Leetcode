afb3f46cf70677e6000622a6d65e4c156a843d87
namaT3003 <n.tiwari0138@gmail.com>
2026-07-29 22:13:56 +0530

    Time: 0 ms (100%), Space: 42.9 MB (67.14%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String toLowerCase(String s) {
+         StringBuilder ans = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c >= 'A' && c <= 'Z') {
+                ans.append((char)(c + 32));
+            } else {
+                ans.append(c);
+            }
+        }
+
+        return ans.toString();
+    }
+    
+}
